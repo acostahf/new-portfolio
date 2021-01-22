2d6bf2a6fa2577c686afb0ec250f4ca701298a16
acostahf <acostahf4@gmail.com>
2021-01-21 22:33:21 -0600

    adding in gsap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 import "fontsource-roboto";
 import NavBar from "./components/NavBar";
@@ -5,10 +6,12 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
-import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
 import { makeStyles } from "@material-ui/core/styles";
 import Projects from "./components/Projects";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,24 +22,39 @@ const useStyles = makeStyles((theme) => ({
     // })`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+    // overflow: "hidden",
   },
 }));
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+  });
+
+  let about = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.core.globals("ScrollTrigger", ScrollTrigger);
+      console.log("works");
+    }
+  });
+
   const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
       <div className={classes.root}>
-        <CssBaseline />
         <NavBar />
         <Hero />
         <About />
         <Experience />
         <Projects />
-        <Footer />
+        <Footer ref={(el) => (about = el)} />
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
